634e4a9f25b1364abff6156365692512298db1ca
jaesung.ko <playme89@naver.com>
2023-01-27 16:16:08 +0900

    Initial commit

---
+++ src/main/java/com/example/boardproject1/BoardProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.boardproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProject1Application.class, args);
+    }
+
+}
