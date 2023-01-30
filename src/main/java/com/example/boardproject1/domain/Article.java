a917b92b91cce40e353e23858e79d72811565c35
jaesung.ko <playme89@naver.com>
2023-01-30 15:48:55 +0900

    오늘의 중간점검

---
+++ src/main/java/com/example/boardproject1/domain/Article.java
@@ -0,0 +1,15 @@
+package com.example.boardproject1.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+}
